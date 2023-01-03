98398b0c9a239b79abaeb7cc9dff80efae18a47a
Murilo Maldonado Mendes <murilomaldonadom@gmail.com>
2023-01-03 14:52:47 -0300

    Day Three

--- src/main/java/com/api/hundreddaysofcode2023/controllers/WeekOneController.java
+++ src/main/java/com/api/hundreddaysofcode2023/controllers/WeekOneController.java
@@ -1,6 +1,7 @@
 package com.api.hundreddaysofcode2023.controllers;
 
 import com.api.hundreddaysofcode2023.services.WeekOneService;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,18 +16,25 @@ public class WeekOneController {
     @Autowired
     WeekOneService weekOneService;
 
-    @GetMapping("/day-one/{age}")
+    @GetMapping("/day1/{age}")
     @ApiOperation("Método que retorna idade em dias")
     public ResponseEntity<Integer> dayOne(@PathVariable Integer age){
         Integer days = weekOneService.daysOfAge(age);
         return ResponseEntity.status(HttpStatus.OK).body(days);
     }
 
-    @GetMapping("/day-two/{text}")
+    @GetMapping("/day2/{text}")
     @ApiOperation("Método que procura a palavra Nemo em uma String")
     public ResponseEntity<String> dayTwo(@PathVariable String text){
 
         String message = weekOneService.findNemo(text);
+        return ResponseEntity.status(HttpStatus.OK).body(message);
+    }
+
+    @GetMapping("/day3/{skewers}")
+    @ApiOperation(value = "Método que recebe uma lista de espetinhos e retorna quantida dos vegetarianos e não vegetarianos", notes = "Exemplo de request: \"--oooo-ooo--\", \"--xx--x--xx--\", \"--o---o--oo--\", \"--xx--x--ox--\", \"--xx--x--ox--\"")
+    public ResponseEntity<String> dayThree(@PathVariable String skewers){
+        String message = weekOneService.vegeterianSkewer(skewers);
         return ResponseEntity.status(HttpStatus.OK).body(message);
     }
 }
--- src/main/java/com/api/hundreddaysofcode2023/services/WeekOneService.java
+++ src/main/java/com/api/hundreddaysofcode2023/services/WeekOneService.java
@@ -2,6 +2,8 @@ package com.api.hundreddaysofcode2023.services;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+
 @Service
 public class WeekOneService {
 
@@ -10,6 +12,7 @@ public class WeekOneService {
         return age * 365;
     }
 
+    //Método responsável por buscar a palavra "nemo" em uma string
     public String findNemo(String text){
         Integer position = text.toLowerCase().indexOf("nemo");
 
@@ -19,5 +22,20 @@ public class WeekOneService {
         }
 
         return message;
+    }
+
+    //Método responsável por distinguir espetinhos vegetarianos (que contém "-o") e não vegetarianos (que contém "-x")
+    public String vegeterianSkewer(String skewers){
+        String[] sk = skewers.split(",");
+        Integer vegSkewers = 0;
+        Integer nonVegSkewers = 0;
+        for (String skewer: sk) {
+            if (skewer.toLowerCase().indexOf("x") == -1) {
+                vegSkewers++;
+            } else {
+                nonVegSkewers++;
+            }
+        }
+        return String.format("Vegetarian Skewers: %s, non vegetarian skewers: %s", vegSkewers, nonVegSkewers);
     }
 }
